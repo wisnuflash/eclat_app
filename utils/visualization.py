deadba14dde07f5479606ba8b9ffb772bb4745e1
wisnuflash <wisnuflash@gmail.com>
2025-06-23 21:28:47 +0700

    update

--- utils/visualization.py
+++ utils/visualization.py
@@ -198,8 +198,12 @@ class VisualizationManager:
         # Create figure
         fig = go.Figure(data=[edge_trace, node_trace],
                        layout=go.Layout(
-                           title='Association Rules Network Graph',
-                           titlefont_size=16,
+                        #    title='Association Rules Network Graph',
+                        #    titlefont_size=16,
+                           title=dict(
+                                text='Association Rules Network Graph',
+                                font=dict(size=16)
+                            ),
                            showlegend=False,
                            hovermode='closest',
                            margin=dict(b=20,l=5,r=5,t=40),
